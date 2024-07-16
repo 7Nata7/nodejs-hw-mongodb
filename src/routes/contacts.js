3d662e7b812384e6294d442ac9fb6a8a9df563e8
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-16 12:12:16 +0300

    delete

--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,25 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const { getAllContacts } = require('../services/contacts');
-
-// router.get('/contacts', async (req, res) => {
-//   try {
-//     const contacts = await getAllContacts();
-//     res.status(200).json({
-//       status: 200,
-//       message: 'Successfully found contacts!',
-//       data: contacts
-//     });
-//   } catch (error) {
-//     res.status(500).json({
-//       message: 'Error fetching contacts',
-//       error: error.message
-//     });
-//   }
-// });
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const { getAllContacts, getContactById } = require('../services/contacts');
