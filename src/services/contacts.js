951a5b65b0da2d9ec25da6a26c4cca5590667aa4
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-17 10:49:11 +0300

    check3

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import Contact from '../models/Contact.js'; // Додайте розширення .js
+import Contact from '../models/Contacts.js';
 
 export const getAllContacts = async () => {
   return await Contact.find({});
