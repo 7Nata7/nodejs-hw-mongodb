f0bb6801e74746b3802c31f170c97cd082245680
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-17 10:21:36 +0300

    check

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import initMongoConnection from './db/initMongoConnection.js'; 
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import initMongoConnection from './db/initMongoConnection.js'; 
 
-dotenv.config();
+// dotenv.config();
+
+// const app = express();
+// const port = process.env.PORT 
+
+// app.use(express.json());
+
+// import contactsRouter from './routes/contacts.js';
+// app.use('/api', contactsRouter);
+
+// initMongoConnection().then(() => {
+//   app.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+//   });
+// }).catch((error) => {
+//   console.error('Failed to initialize MongoDB connection:', error.message);
+// });
+
+import express from 'express';
+import initMongoConnection from './db/initMongoConnection.js';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env.example' });
 
 const app = express();
-const port = process.env.PORT 
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+initMongoConnection();
 
-import contactsRouter from './routes/contacts.js';
-app.use('/api', contactsRouter);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 
-initMongoConnection().then(() => {
-  app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  });
-}).catch((error) => {
-  console.error('Failed to initialize MongoDB connection:', error.message);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
