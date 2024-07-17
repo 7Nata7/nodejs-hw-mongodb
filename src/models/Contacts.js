4d40a988f05b8d9f63c7f3876f94b47ddaf0c3bc
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-17 11:00:28 +0300

    check5

--- src/models/Contacts.js
+++ src/models/Contacts.js
@@ -1,18 +1,32 @@
+// import mongoose from 'mongoose';
+
+// const contactSchema = new mongoose.Schema({
+//   name: {
+//     type: String,
+//     required: true,
+//   },
+//   phone: {
+//     type: String,
+//     required: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//   },
+// });
+
+// const Contact = mongoose.model('Contact', contactSchema);
+
+// export default Contact;
+
 import mongoose from 'mongoose';
 
 const contactSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
+  name: String,
+  email: String,
+  phone: String,
+}, {
+  timestamps: true
 });
 
 const Contact = mongoose.model('Contact', contactSchema);
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,9 +1,19 @@
-import Contact from '../models/Contacts.js';
+// import Contact from '../models/Contacts.js';
+
+// export const getAllContacts = async () => {
+//   return await Contact.find({});
+// };
+
+// export const getContactById = async (id) => {
+//   return await Contact.findById(id);
+// };
+
+import ContactModel from '../models/Contacts.js';
 
 export const getAllContacts = async () => {
-  return await Contact.find({});
+  return await ContactModel.find();
 };
 
-export const getContactById = async (id) => {
-  return await Contact.findById(id);
+export const getContactById = async (contactId) => {
+  return await ContactModel.findById(contactId);
 };
