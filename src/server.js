cc49b7d9c35594a806b1b8625be3592a52c6850c
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-17 11:27:18 +0300

    check6

---
+++ src/server.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino';
+import dotenv from 'dotenv';
+import contactsRouter from './routes/contacts.js';
+import initMongoConnection from './db/initMongoConnection.js';
+
+dotenv.config({ path: '.env.example' });
+
+const logger = pino();
+
+const setupServer = async () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(express.json());
+
+  app.use((req, res, next) => {
+    logger.info(`${req.method} ${req.url}`);
+    next();
+  });
+
+  app.use('/api', contactsRouter);
+
+  app.use((req, res) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  const PORT = process.env.PORT || 3000;
+
+  await initMongoConnection();
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+export default setupServer;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,13 +1,3 @@
-// import Contact from '../models/Contacts.js';
-
-// export const getAllContacts = async () => {
-//   return await Contact.find({});
-// };
-
-// export const getContactById = async (id) => {
-//   return await Contact.findById(id);
-// };
-
 import ContactModel from '../models/Contacts.js';
 
 export const getAllContacts = async () => {
