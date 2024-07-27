75f70efc49df036bcc28859f510b90e5b58821ea
7Nata7 <119401527+7Nata7@users.noreply.github.com>
2024-07-27 10:57:08 +0300

    9

--- src/db/Contacts.js
+++ src/db/Contacts.js
@@ -1,27 +1,3 @@
-// import mongoose from 'mongoose';
-
-// const { Schema } = mongoose;
-
-// const contactSchema = new Schema({
-//   name: {
-//     type: String,
-//     required: [true, 'Name is required'],
-//   },
-//   email: {
-//     type: String,
-//     required: [true, 'Email is required'],
-//     unique: true,
-//   },
-//   phone: {
-//     type: String,
-//     required: [true, 'Phone is required'],
-//   },
-// });
-
-// const Contact = mongoose.model('Contact', contactSchema);
-
-// export default Contact;
-
 import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
--- src/routes/contacts.js
+++ src/routes/contacts.js
@@ -1,16 +1,3 @@
-// import { Router } from 'express';
-// import contactsController from '../controllers/contacts.js';
-
-// const router = Router();
-
-// router.get('/', contactsController.getAllContacts);
-// router.get('/:contactId', contactsController.getContactById);
-// router.post('/', contactsController.createContact);
-// router.patch('/:contactId', contactsController.updateContact);
-// router.delete('/:contactId', contactsController.deleteContact);
-
-// export default router;
-
 import { Router } from 'express';
 import contactsController from '../controllers/contacts.js';
 
